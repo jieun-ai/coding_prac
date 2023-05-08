987be42cafdb7a5d0188ca2b46ecd2d05853388e
jieun-ai <qweasd921@naver.com>
2023-05-08 23:48:49 +0900

    연결리스트-2 강의_0508

--- 4_append.py
+++ 4_append.py
@@ -18,3 +18,24 @@ li.append(1)
 li.append(2)
 li.append(3)
 li.append(4)
+
+####################################################################################
+
+class LinkedList(object):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = self.tail.next
+
+li = LinkedList()
+li.append(1)
+li.append(2)
+li.append(3)
+li.append(4)
---
+++ 7_remove.py
@@ -0,0 +1,8 @@
+def remove(self, idx):
+    if idx == 0:
+        self.head = self.head.next # garbage collector가 알아서 처리해준다.
+    else:
+        current = self.head
+        for _ in range(idx-1):
+            current = current.next
+        current.next = current.next.next
